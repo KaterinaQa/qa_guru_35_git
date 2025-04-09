47f322fd1491cd350e458e8349a82bc6e8e838f8
katerinaqa <knedostaeva@sbdagroup.com>
2025-04-09 20:09:50 +0300

    initial commit

---
+++ src/test/java/com/negtag7/DemoTest.java
@@ -0,0 +1,16 @@
+package com.negtag7;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+    @Test
+    void firstTest() {
+        Assertions.assertTrue(3 >= 2);
+    }
+
+    @Test
+    void secondTest() {
+        Assertions.assertTrue(3 >= 1);
+    }
+}
